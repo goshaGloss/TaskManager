c64cb7a475bf27e26d462e559fb071cf94c3873f
goshaGloss <secacctwink@gmail.com>
2021-06-21 00:10:16 +0600

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="onClick()" class="btn">
+        wtf
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+    text:String,
+    color:String
+    },
+    methods:{
+    onClick() {
+        this.$emit('show_btn')
+    }
+}
+}
+
+
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>sup guys</h1>
+        <Button @show_btn = "$emit('show_btn')"/>
+    </header>
+</template>
+
+
+
+<script>
+import Button from './Button'
+
+export default {
+    name:'Header',
+    props:{
+        title:String,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+ header{
+     display: flex;
+     justify-content: space-between;
+     margin-bottom: 10px;
+     align-items: center;
+}
+</style>
